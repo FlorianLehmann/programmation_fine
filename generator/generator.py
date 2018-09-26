afef0a0fe1c988d540484dd6c7f11a96b023dcfd
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-09-26 09:58:10 +0200

    Add constant, sorted and reversed generator.

--- generator/generator.py
+++ generator/generator.py
@@ -1,6 +1,20 @@
 import random
 
 
+def constant_array(n, value=None):
+    if value is None:
+        value = random.randrange(1000)
+    return [value for _ in range(n)]
+
+
 def random_array(n, max_value=1000):
     return [random.randrange(max_value) for _ in range(n)]
+
+
+def sorted_array(n, max_value=1000):
+    return sorted(random_array(n, max_value))
+
+
+def reverse_sorted_array(n, max_value=1000):
+    return reversed(sorted_array(n, max_value))
 
