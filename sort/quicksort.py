9b0d0e5ac3a20d04e7e58429f668f3261cd50068
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-10-02 19:34:19 +0200

    Add the 3 values median pivot search for the quick sort.

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -2,8 +2,19 @@ def find_pivot(lst, low, high):
 	return lst[high]
 
 
+def find_pivot_by_median_of_3_values(lst, low, high):
+	mid = (low + high) // 2
+	if lst[mid] < lst[low]:
+		lst[low], lst[mid] = lst[mid], lst[low]
+	if lst[high] < lst[low]:
+		lst[low], lst[high] = lst[high], lst[low]
+	if lst[mid] < lst[high]:
+		lst[low], lst[high] = lst[high], lst[low]
+	return lst[high]
+
+
 def partition(lst, low, high):
-	pivot = find_pivot(lst, low, high)
+	pivot = find_pivot_by_median_of_3_values(lst, low, high)
 	i = low
 	for j in range(low, high):
 		if lst[j] < pivot:
