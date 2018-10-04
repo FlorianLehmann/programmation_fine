8c6b36f4e77c8942527422819261b4a99b3bead4
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-10-04 20:54:29 +0200

    Fix fusion sort.

--- sort/__init__.py
+++ sort/__init__.py
@@ -3,4 +3,22 @@ from sort.fusion import fusion
 from sort.heapsort import heap_sort
 from sort.quicksort import quick_sort
 
-sorting_algorithms = [insertion, fusion, heap_sort, quick_sort]
+
+def python_native_sort_in_place(lst):
+	lst.sort()
+	return lst
+
+
+def python_native_not_in_place(lst):
+	return sorted(lst)
+
+
+sorting_algorithms = [
+	python_native_sort_in_place,
+	python_native_not_in_place,
+	insertion,
+	fusion,
+	heap_sort,
+	# quicksort
+]
+
--- sort/fusion.py
+++ sort/fusion.py
@@ -1,20 +1,27 @@
+
+
 def fusion(lst):
-    length = len(lst)
-    # Si la liste contient 0 ou 1 élément, alors elle est naturellement triée
-    if length <= 1:
-        return lst
-    # On cherche l'indice median de la liste
-    median = length // 2
-    # On sépare la liste en deux et on appel la fonction merge sur les deux sous-parties
-    return merge(fusion(lst[:median]), fusion(lst[median:]))
+    buffer = lst[:]
+    _fusion(lst, buffer, 0, len(lst))
+    return lst
 
 
-def merge(a, b):
-    if not a:
-        return b
-    if not b:
-        return a
-    if a[0] <= b[0]:
-        return [a[0]] + merge(a[1:], b)
-    return [b[0]] + merge(a, b[1:])
+def _fusion(lst, buffer, start, end):
+    if end - start <= 1:
+        return
+    median = (start + end) // 2
+    _fusion(buffer, lst, start, median)
+    _fusion(buffer, lst, median, end)
+    merge(lst, buffer, start, median, end)
 
+
+def merge(lst, buffer, start, median, end):
+    x = start
+    y = median
+    for i in range(start, end):
+        if y >= end or (x < median and buffer[x] < buffer[y]):
+            lst[i] = buffer[x]
+            x += 1
+        else:
+            lst[i] = buffer[y]
+            y += 1
